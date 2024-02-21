79d897be6f26aa5d16ff192fb4acdf36ab8a4498
MDWojcieszak <wojcieszak.4@gmail.com>
2024-02-21 23:13:20 +0100

    feature(appProvider): user provider

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react';
 import { ThemeProvider } from './contexts/Theme/ThemeProvider';
 import { Theme } from './utils/theme';
+import { ModalManagerProvider } from '~/contexts/ModalManager/ModalManagerProvider';
+import { UserProvider } from '~/contexts/User/UserProvider';
 
 type AppProviderProps = {
   theme: Theme;
@@ -8,5 +10,11 @@ type AppProviderProps = {
 };
 
 export const AppProvider = (p: AppProviderProps) => {
-  return <ThemeProvider theme={p.theme}>{p.children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={p.theme}>
+      <UserProvider>
+        <ModalManagerProvider>{p.children}</ModalManagerProvider>
+      </UserProvider>
+    </ThemeProvider>
+  );
 };
