e8010bc34c5fc648bd0b962606562feaa9fc5e86
MDWojcieszak <wojcieszak.4@gmail.com>
2024-02-25 23:50:15 +0100

    feature(routes): end to end image form

--- src/routes/Images/modals/CreateImageModal.tsx
+++ src/routes/Images/modals/CreateImageModal.tsx
@@ -1,12 +1,18 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { FileService } from '~/api/File';
 import { Button } from '~/components/Button';
+import { MonthYearInput } from '~/components/MonthYearInput';
 import { ImageInputWithPreview } from '~/components/ImageInputWithPreview';
 import { Input } from '~/components/Input';
+import { SideMotionContainer, SideMotionContainerRef } from '~/components/SideMotionContainer';
 import { mkUseStyles } from '~/utils/theme';
 import { validateFile } from '~/utils/validation/fileValidation';
+import { TextArea } from '~/components/TextArea';
+import { ImageService } from '~/api/Image';
+import { InternalModalProps } from '~/contexts/ModalManager/types';
 
 const ImageSchema = z.object({
   image: z.instanceof(File, { message: 'Image is required' }).superRefine((file, ctx) => validateFile(file, ctx)),
@@ -14,20 +20,27 @@ const ImageSchema = z.object({
 type ImageSchemaType = z.infer<typeof ImageSchema>;
 
 const ImageDataSchema = z.object({
-  localization: z.string(),
+  localization: z.string({ required_error: 'Localization is required' }).min(1, 'Localization is required'),
   dateTaken: z.date(),
 
   title: z.string().optional(),
   description: z.string().optional(),
 });
 type ImageDataSchemaType = z.infer<typeof ImageDataSchema>;
-type Step = 'image' | 'imageData';
+enum StepCards {
+  IMAGE = 'image',
+  IMAGE_DATA = 'image_data',
+}
 
-export const CreateImageModal = () => {
+type CreateImageModalProps = Partial<InternalModalProps>;
+
+export const CreateImageModal = (p: CreateImageModalProps) => {
+  const [imageUploadLoading, setImageUploadLoading] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [step, setStep] = useState<Step>('image');
-  const styles = useStyles();
 
+  const [imageId, setImageId] = useState<string>();
+  const styles = useStyles();
+  const containerRef = useRef<SideMotionContainerRef>(null);
   const imageFormMethods = useForm<ImageSchemaType>({
     resolver: zodResolver(ImageSchema),
   });
@@ -36,20 +49,38 @@ export const CreateImageModal = () => {
     resolver: zodResolver(ImageDataSchema),
   });
 
-  const handleSelectImage = (data: ImageSchemaType) => {
-    setStep('imageData');
+  const handleUploadImage = async (data: ImageSchemaType) => {
+    if (imageId) return containerRef.current?.setActiveCard(StepCards.IMAGE_DATA);
+    setImageUploadLoading(true);
+
+    try {
+      const response = await FileService.uploadImage(data.image);
+      setImageId(response.id);
+      containerRef.current?.setActiveCard(StepCards.IMAGE_DATA);
+    } catch (e) {
+      imageFormMethods.setError('image', { message: 'Error uploading image' });
+    }
+    setImageUploadLoading(false);
   };
 
-  const handleSubmit = (data: ImageDataSchemaType) => {
-    console.log(data);
+  const handleSubmit = async (data: ImageDataSchemaType) => {
+    if (!imageId) return;
+    setLoading(true);
+    try {
+      await ImageService.create({ ...data, imageId });
+      p.handleClose?.();
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
   };
 
   return (
-    <>
+    <SideMotionContainer ref={containerRef} cards={[StepCards.IMAGE, StepCards.IMAGE_DATA]} cardWidth={400}>
       <FormProvider {...imageFormMethods}>
-        <form style={styles.form} onSubmit={imageFormMethods.handleSubmit(handleSelectImage)} noValidate>
+        <form style={styles.form} onSubmit={imageFormMethods.handleSubmit(handleUploadImage)} noValidate>
           <ImageInputWithPreview name='image' description='Select Image' />
-          <Button label='Next' loading={loading} style={styles.button} />
+          <Button label='Next' loading={imageUploadLoading} style={styles.button} />
         </form>
       </FormProvider>
       <FormProvider {...imageDataFormMethods}>
@@ -60,16 +91,24 @@ export const CreateImageModal = () => {
             description='Enter the location where the photo was taken'
             type='string'
           />
+          <MonthYearInput label='Date' name='dateTaken' description='Select the month and year the photo was taken' />
           {/* TODO: add date selector */}
           <Input name='title' label='Title' description='Enter photo title' type='string' />
-          <Input name='description' label='Description' description='Enter photo description' type='string' />
+          <TextArea name='description' label='Description' description='Enter photo description' type='string' />
+
           <div style={styles.row}>
-            <Button label='Back' variant='secondary' style={styles.button} />
+            <Button
+              label='Back'
+              variant='secondary'
+              onClick={() => containerRef.current?.setActiveCard('image')}
+              style={styles.button}
+              type='button'
+            />
             <Button label='Create' loading={loading} style={styles.button} />
           </div>
         </form>
       </FormProvider>
-    </>
+    </SideMotionContainer>
   );
 };
 
@@ -78,7 +117,6 @@ const useStyles = mkUseStyles((t) => ({
     margin: 'auto',
     alignSelf: 'center',
     padding: t.spacing.m,
-    minWidth: 500,
   },
   form: {
     display: 'flex',
