c040dec5fe5805a67e77d491bf5ad90c86bc0c57
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-19 23:54:02 +0100

    feature(api): server types

---
+++ src/api/Server.ts
@@ -0,0 +1,72 @@
+import { z } from 'zod';
+import { api } from '~/adapters/api';
+import { ApiTag } from '~/api/types';
+
+const DiskType = z.object({
+  available: z.number(),
+  fs: z.string(),
+  id: z.string(),
+  mediaType: z.string().optional().nullable(),
+  name: z.string().optional().nullable(),
+  type: z.string(),
+  used: z.number(),
+});
+export type DiskType = z.infer<typeof DiskType>;
+
+const MemoryType = z.object({
+  free: z.number(),
+  id: z.string(),
+  total: z.number(),
+});
+export type MemoryType = z.infer<typeof MemoryType>;
+
+const CpuType = z.object({
+  cores: z.number(),
+  physicalCores: z.number(),
+  currentLoad: z.number(),
+  currentLoadSystem: z.number(),
+  currentLoadUser: z.number(),
+  id: z.string(),
+});
+export type CpuType = z.infer<typeof CpuType>;
+
+const ServerPropertiesType = z.object({
+  status: z.string(),
+  uptime: z.number(),
+  diskInfo: z.array(DiskType),
+  memoryInfo: MemoryType,
+  cpuInfo: CpuType,
+});
+export type ServerPropertiesType = z.infer<typeof ServerPropertiesType>;
+
+const ServerType = z.object({
+  id: z.string(),
+  name: z.string(),
+  ipAddress: z.string(),
+  location: z.string().optional().nullable(),
+  properties: ServerPropertiesType,
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+});
+export type ServerType = z.infer<typeof ServerType>;
+
+const ServerDataResponse = z.object({
+  total: z.number(),
+  servers: z.array(ServerType),
+});
+
+export type ServerDataResponse = z.infer<typeof ServerDataResponse>;
+
+export const ServerService = {
+  tag: ApiTag.SERVER,
+
+  async getAll(): Promise<ServerDataResponse> {
+    try {
+      const response = await api(ServerService.tag).get('all');
+      return ServerDataResponse.parse(response);
+    } catch (error) {
+      console.error('Error getting server list:', error);
+      throw error;
+    }
+  },
+};
--- src/api/types.ts
+++ src/api/types.ts
@@ -6,6 +6,7 @@ export enum ApiTag {
   IMAGE = 'image',
   SESSION = 'session',
   FILE = 'file',
+  SERVER = 'server',
 }
 
 export enum Auth {
