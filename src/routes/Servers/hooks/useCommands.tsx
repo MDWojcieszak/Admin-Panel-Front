c9ed396a33446235b5fe9c4cdb63a9484c5920c9
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-29 23:51:52 +0100

    feature(routes): use commands web socket

--- src/routes/Servers/hooks/useCommands.tsx
+++ src/routes/Servers/hooks/useCommands.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { CommandType, ServerCommandsService } from '~/api/ServerCommands';
+import useWebSocket from '~/hooks/useWebSocket';
 
 export const useCommands = (idCategory: string) => {
   const [commands, setCommands] = useState<CommandType[]>();
   const [loading, setLoading] = useState(false);
+
   const handleGet = async () => {
     if (!idCategory) return;
     try {
@@ -14,6 +16,9 @@ export const useCommands = (idCategory: string) => {
       setLoading(false);
     }
   };
+
+  useWebSocket('server-command.update', handleGet);
+
   useEffect(() => {
     handleGet();
   }, [idCategory]);
